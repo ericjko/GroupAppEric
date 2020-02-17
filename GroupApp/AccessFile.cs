6d6b930ba04966b1cf96213a375b2e1313bc48e0
Eric Ko <ericjko@yahoo.com>
2020-02-17 14:26:56 +0000

    implemented login page

---
+++ GroupApp/AccessFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace GroupApp
+{
+    class AccessFile
+    {
+        public static string FilePath(string file)
+        {
+            return System.IO.Path.Combine(FileSystem.AppDataDirectory, file);
+        }
+    }
+}
--- GroupApp/App.xaml.cs
+++ GroupApp/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.IO;
+using GroupApp;
 using GroupApp.Data;
+using GroupApp.DB;
+using GroupApp.LoginPage;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -8,11 +11,25 @@ namespace GroupApp
 {
     public partial class App : Application
     {
+        public static string usercheck;
+        string Database => AccessFile.FilePath("db");
+        public static UserDatabase UserDB { get; private set; }
         public App()
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new MapsPage());
+            //MainPage = new NavigationPage(new MapsPage());
+            MainPage = new NavigationPage(new Login());
+
+            UserDB = new UserDatabase(Database);
+        }
+        public string getusercheck()
+        {
+            return usercheck;
+        }
+        public void setusercheck(string name)
+        {
+            usercheck = name;
         }
         static PinDatabase pindatabase;
 
@@ -22,7 +39,7 @@ namespace GroupApp
             {
                 if (pindatabase == null)
                 {
-                    pindatabase = new PinDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Pin11.db3"));
+                    pindatabase = new PinDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Pin12.db3"));
                 }
                 return pindatabase;
             }

bca9b2ed456a6f746ff4f105e870db2d4b9eb239
Isa-Muhammad <36093501+Isa-Muhammad@users.noreply.github.com>
2020-02-28 16:53:49 +0000

    * App.xaml.cs: * MapsPage.xaml: * GroupApp.csproj: * ProfilePage.xaml: * CategoryPage.xaml: * ClubData.cs: * ProfilePage.xaml.cs: * CategoryPage.xaml.cs: * GroupApp.iOS.csproj: * DeviceOrientation.cs: * CategoryViewModel.cs:
    * PhotoPickerService.cs: Profile picker service
    
    * IPhotoPickerService.cs: Implement photo picker service

--- GroupApp/ProfilePage.xaml.cs
+++ GroupApp/ProfilePage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.IO;
+using GroupApp.Services;
 using Xamarin.Forms;
 
 namespace GroupApp
@@ -10,6 +11,19 @@ namespace GroupApp
         public ProfilePage()
         {
             InitializeComponent();
+        }
+
+        async void OnPickPhotoButtonClicked(object sender, EventArgs e)
+        {
+            (sender as Button).IsEnabled = false;
+
+            Stream stream = await DependencyService.Get<IPhotoPickerService>().GetImageStreamAsync();
+            if (stream != null)
+            {
+                image.Source = ImageSource.FromStream(() => stream);
+            }
+
+            (sender as Button).IsEnabled = true;
         }
     }
 }
